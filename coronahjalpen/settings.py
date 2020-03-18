37da4104883aaa2e3b109343653e8f98b098b0d6
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2020-03-18 15:08:51 -0700

    add allowed hosts

--- coronahjalpen/settings.py
+++ coronahjalpen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'web', 'coronahjalp-habo.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'web', 'coronahjalp-habo.herokuapp.com', 'www.coronahjalp-habo.se', 'coronahjalp-habo.se']
 
 
 # Application definition
